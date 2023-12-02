8e6546c760c4f6f2a05ea330c152d6aa7da5de8f
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-02 19:12:28 +0300

    Modified the content

--- app/components/home/WhatWeDo.tsx
+++ app/components/home/WhatWeDo.tsx
@@ -4,6 +4,11 @@ import { WhatWeDoCard } from "./WhatWeDoCard";
 export default function WhatWeDo() {
   return (
     <section className="py-14 px-2 container mx-auto grid gap-14">
+      <div>
+        <h2 className=" text-green-700  font-semibold text-3xl lg:text-4xl text-center">
+          What We Do
+        </h2>
+      </div>
       <div className="grid md:grid-cols-3 gap-4">
         {content.map((card, index: number) => {
           return (
