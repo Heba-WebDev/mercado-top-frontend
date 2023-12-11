3c52a7e155f282045996263642b1d4642ca8df40
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-12 00:52:04 +0300

    Added a state to check if user is logged in or not. If that's the case, the user profile picture is shown in the navbar. If not, a sign up button/link is show

--- app/components/globals/NavbarMobile.tsx
+++ app/components/globals/NavbarMobile.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Overlock } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useAppSelector } from "@/app/hooks/store";
+import { IMAGE_URL } from "@/app/utils/ImageBaseURL";
+import UserDropDown from "./UserDropDown";
 const overlock = Overlock({
   subsets: ["latin"],
   weight: ["400", "700", "900"],
 });
 
 export default function NavbarMobile() {
+  const [hasMounted, setHasMounted] = useState(false);
+  const [openUserDropDown, setOpenUserDropDown] = useState(false);
+  const user = useAppSelector((state) => state.users);
   const [isNavOpen, setIsNavOpen] = useState(false);
   const handleClick = () => {
     setIsNavOpen(!isNavOpen);
@@ -19,6 +25,12 @@ export default function NavbarMobile() {
     pathname === "/signup" ||
     pathname === "/signin" ||
     pathname === "/reset-password";
+  const handleUserDropDown = () => {
+    setOpenUserDropDown(!openUserDropDown);
+  };
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
   if (auth) {
     return (
       <nav className="relative md:hidden flex items-center justify-between container mx-auto py-4 px-2">
@@ -40,48 +52,99 @@ export default function NavbarMobile() {
   }
   return (
     <nav className="relative md:hidden flex items-center justify-between container mx-auto py-4 px-2">
-      <Link
-        href="/"
-        className={`${overlock.className} uppercase flex items-center`}
-      >
-        <Image
-          src="/images/globals/logo.svg"
-          alt="mercado top"
-          width={25}
-          height={50}
-          style={{ width: "25px", height: "50px" }}
-        />
-        <span className=" font-black text-lg">MercadoTop</span>
-      </Link>
-      <button onClick={handleClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
+      <div className=" flex flex-row-reverse items-center gap-1">
+        <Link
+          href="/"
+          className={`${overlock.className} uppercase flex items-center`}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          <Image
+            src="/images/globals/logo.svg"
+            alt="mercado top"
+            width={25}
+            height={50}
+            style={{ width: "25px", height: "50px" }}
           />
-        </svg>
-      </button>
+          <span className=" font-black text-lg">MercadoTop</span>
+        </Link>
+
+        <button onClick={handleClick}>
+          {!isNavOpen && (
+            <Image
+              src="/images/globals/menu-burger.svg"
+              alt="open"
+              width={20}
+              height={10}
+              style={{ width: "20px", height: "auto" }}
+            />
+          )}
+          {isNavOpen && (
+            <Image
+              src="/images/globals/x.svg"
+              alt="open"
+              width={20}
+              height={10}
+              style={{ width: "20px", height: "auto" }}
+            />
+          )}
+        </button>
+      </div>
+      {hasMounted && user.length !== 0 && (
+        <div className="relative">
+          <button onClick={handleUserDropDown}>
+            <Image
+              src={`${IMAGE_URL}${user[0].profile_picture}`}
+              alt=""
+              width={30}
+              height={10}
+              style={{ width: "auto", height: "auto" }}
+              className=" rounded-full"
+            />
+          </button>
+          {openUserDropDown && <UserDropDown />}
+        </div>
+      )}
+
       <div
         className={`${
           isNavOpen ? "block z-40 bg-white" : "hidden"
-        } absolute top-12 w-[96.5%] py-8 shadow-lg rounded-lg`}
+        } absolute top-16 w-[96.5%] py-8 shadow-xl rounded-lg border border-gray-50`}
       >
         <ul className="flex flex-col items-center gap-8">
-          <li>Products</li>
-          <li>How it works</li>
-          <li>About Us</li>
+          <Link href="/market" className="">
+            <li
+              className="border-4
+          border-x-0 border-t-0 border-b-white hover:border-4
+          hover:border-x-0 hover:border-t-0 hover:border-b-[#33A077]"
+            >
+              Market
+            </li>
+          </Link>
+          <Link href="/#what-we-do">
+            <li
+              className="border-4
+          border-x-0 border-t-0 border-b-white hover:border-4
+          hover:border-x-0 hover:border-t-0 hover:border-b-[#33A077]"
+            >
+              What We Do
+            </li>
+          </Link>
+          <Link href="/#about-us">
+            <li
+              className="border-4
+          border-x-0 border-t-0 border-b-white hover:border-4
+          hover:border-x-0 hover:border-t-0 hover:border-b-[#33A077]"
+            >
+              About Us
+            </li>
+          </Link>
         </ul>
-        <div className="flex justify-center py-6">
-          <button>Sign up</button>
-        </div>
+        {hasMounted && user.length == 0 && (
+          <div className="flex justify-center py-6 bg-[#33A077] max-w-[180px] max-h-4 gird items-center rounded-md mx-auto mt-6">
+            <Link href="/signup" className="text-white text-center">
+              Sign up
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
