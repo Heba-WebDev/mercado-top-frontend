a5dd664ebdb394b0731863c141eb124c2c805e80
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-13 13:43:26 +0300

    Added Footer links, social media and copy right components

--- app/components/globals/Footer.tsx
+++ app/components/globals/Footer.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Overlock } from "next/font/google";
+import FooterLinks from "./FooterLinks";
+import FooterSocialMedia from "./FooterSocialMedia";
+import FooterCopyRight from "./FooterCopyRight";
 const overlock = Overlock({
   subsets: ["latin"],
   weight: ["400", "700", "900"],
@@ -18,9 +21,9 @@ export default function Footer() {
     return null;
   }
   return (
-    <footer className=" bg-black text-white py-4 px-2 mt-auto">
+    <footer className=" bg-black text-white py-16 px-2 mt-auto">
       <div className=" container mx-auto flex gap-4 md:gap-0 flex-col md:flex-row md:justify-between md:items-center">
-        <div className=" text-[#33A077] mx-auto md:mx-0">
+        <div className=" text-[#33A077] flex items-center justify-center md:justify-start mx-auto md:mx-0 w-full">
           <Link
             href="/"
             className={`${overlock.className} uppercase flex items-center mt-2`}
@@ -35,12 +38,15 @@ export default function Footer() {
             <span className=" font-black text-lg">MercadoTop</span>
           </Link>
         </div>
-        <div className="text-[#666] text-center md:text-left">
-          Image by{" "}
-          <a href="https://www.freepik.com/free-psd/3d-illustration-person-with-glasses_27470336.htm?query=default%20user#from_view=detail_alsolike">
-            Freepik
-          </a>
-          <span>All rights reserved © {new Date().getFullYear()}</span>
+        <div
+          className=" flex flex-col md:flex-row justify-between
+        gap-8 md:gap-0 w-full"
+        >
+          <FooterLinks />
+          <div className="w-full">
+            <FooterSocialMedia />
+            <FooterCopyRight />
+          </div>
         </div>
       </div>
     </footer>
