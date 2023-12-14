24ad4b127afc5956550bacceb37e6eebb15bd812
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-14 23:20:35 +0300

    Re-designed the UI

--- app/(pages)/market/components/AddAProduct.tsx
+++ app/(pages)/market/components/AddAProduct.tsx
@@ -2,32 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 export default function AddAProduct() {
   return (
-    <div className="relative max-h-[450px] overflow-hidden xl:mx-auto xl:container">
-      <div className=" absolute top-[37%] w-full flex items-center justify-center">
-        <h1 className=" text-3xl md:text-5xl shadow-black xl:text-5xl text-white font-bold  text-center max-w-[20ch] ">
+    <div className="relative container mx-auto py-24 text-center grid gap-4 justify-center">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl md:text-5xl font-bold">
           You no longer need it?
-          <Link href="" className=" text-[#33A077] underline px-2 relative">
-            Sell
-            <Image
-              src="/images/market/arrow-up.svg"
-              alt="click on sell"
-              width={30}
-              height={10}
-              className=" absolute top-10 md:top-16 left-5 md:left-8 bg-white rounded-full"
-            />
-          </Link>
-          it on MercadoTop{" "}
         </h1>
+        <p>Sell any item you have at home on Mercado Top</p>
       </div>
-
-      <Image
-        src="/images/market/photo-boards.jpg"
-        alt=""
-        width={2270}
-        height={100}
-        style={{ width: "auto", height: "auto", objectFit: "contain" }}
-        className=""
-      />
+      <button
+        className=" bg-[#33A077] hover:bg-[#227356]
+      py-3  px-1 text-white rounded-lg w-64 mx-auto uppercase font-medium"
+      >
+        Sell now
+      </button>
     </div>
   );
 }
