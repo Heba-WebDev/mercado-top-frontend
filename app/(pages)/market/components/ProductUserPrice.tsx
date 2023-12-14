0381987e2756fa1dc91a5ad72c6397ba4a1310a4
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-14 23:24:45 +0300

    Changed the css styles

--- app/(pages)/market/components/ProductUserPrice.tsx
+++ app/(pages)/market/components/ProductUserPrice.tsx
@@ -6,43 +6,21 @@ export interface details {
   price: string;
 }
 
-export default function ProductUserPrice({
-  user,
-  date,
-  country,
-  price,
-}: details) {
+export default function ProductUserPrice({ user, date, country }: details) {
   return (
     <div className="py-4 flex items-center justify-between w-full">
-      <div className="w-full text-xs text-[#666] grid gap-2">
-        <div className="flex items-center gap-1">
-          <Image src="/images/market/user.svg" alt="" width={15} height={30} />
-          <p>random_12</p>
-        </div>
-        <div className="flex items-center gap-1">
-          <Image src="/images/market/time.svg" alt="" width={15} height={30} />
-          <p>2 Days ago</p>
-        </div>
+      <div className="flex items-center gap-1 text-xs text-[#666] w-full">
+        <Image src="/images/market/user.svg" alt="" width={15} height={30} />
+        <p>random_12</p>
       </div>
-      <div className="flex flex-col items-center gap-2 pr-1">
-        <div className="flex items-center justify-center gap-1 text-xs text-[#666] w-full">
-          <Image
-            src="/images/market/MapPin.svg"
-            alt=""
-            width={15}
-            height={30}
-          />
-          <p>{country}</p>
-        </div>
-        <div className="flex items-center justify-start gap-1 text-sm text-[#666] w-full">
-          <Image
-            src="/images/market/price-tag.svg"
-            alt=""
-            width={15}
-            height={30}
-          />
-          <p>{price}</p>
-        </div>
+      <div className="flex items-center justify-center gap-1 text-xs text-[#666] w-full">
+        <Image src="/images/market/time.svg" alt="" width={15} height={30} />
+        <p>2 Days ago</p>
+      </div>
+
+      <div className="flex items-center justify-end gap-1 text-xs text-[#666] w-full">
+        <Image src="/images/market/MapPin.svg" alt="" width={15} height={30} />
+        <p>{country}</p>
       </div>
     </div>
   );
