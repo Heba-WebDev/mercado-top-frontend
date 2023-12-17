6d89a133689a6bf3fed4ec9157bb2c35e474b278
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-17 21:00:25 +0300

    Changed the extension from tsx to ts

--- app/utils/CustomError.tsx
+++
@@ -1,7 +0,0 @@
-export class CustomError extends Error {
-  constructor(public response: { data: { message: string } }) {
-    super(response.data.message);
-    this.isCustomError = true;
-  }
-  isCustomError: boolean;
-}
