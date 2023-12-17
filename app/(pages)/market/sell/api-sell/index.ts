d230996b1db35490cc1b528cedb64c3e72ee56ab
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-17 20:56:27 +0300

    Created the function that fetch the create proudct end point

---
+++ app/(pages)/market/sell/api-sell/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { CustomError } from "@/app/utils/CustomError";
+import { ApiAxiosInterceptor } from "@/app/react-query/axios";
+import { ICreateProduct } from "../types";
+
+export const createProduct = async (data: FormData) => {
+  try {
+    const response = await ApiAxiosInterceptor.post("/api/products/", data);
+    if (response.status >= 200 && response.status < 300) {
+      return response.data;
+    } else {
+      throw new CustomError(response);
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error) && error?.response) {
+      throw new CustomError(error.response);
+    } else {
+      throw error;
+    }
+  }
+};
