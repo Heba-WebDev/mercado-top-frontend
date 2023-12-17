d3229aa9b39be6186a0313b1a1b86bf8e9ce3e22
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-17 20:57:23 +0300

    Created a folder for interfaces of sell route

---
+++ app/(pages)/market/sell/interfaces/index.ts
@@ -0,0 +1,11 @@
+
+export interface FormValues {
+  user_id: string;
+  country: string;
+  title: string;
+  description: string;
+  category_id: number;
+  price: number;
+  currency: number;
+  photo_1: File | null;
+}
