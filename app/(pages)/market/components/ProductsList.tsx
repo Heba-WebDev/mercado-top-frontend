2add53360409572ea87aac07d668e705500bfb73
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-18 19:42:48 +0300

    Updated the component to handle error and loading states

--- app/(pages)/market/components/ProductsList.tsx
+++ app/(pages)/market/components/ProductsList.tsx
@@ -1,19 +1,41 @@
-import ProductCard from "./ProductCard";
+"use client";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/hooks/store";
+import { fetchProducts } from "@/app/store/products/slice";
+import SkeletonCard from "./SkeletonCard";
+import FailureToFetchProducts from "./FailureToFetchProducts";
+import Products from "./Products";
 
 export default function ProductsList() {
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useAppDispatch();
+  const products = useAppSelector((state) => state.products);
+  const status = useAppSelector((state) => state.products?.status);
+  const error = useAppSelector((state) => state.products?.error);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchProducts());
+    }
+    if (status === "succeeded") {
+      setIsLoading(false);
+    }
+  }, [status, dispatch]);
+
   return (
     <section className="mt-12 md:mt-14 md:pt-14 pb-32 grid">
-      <div className="grid md:grid-cols-3 gap-8">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-      <button className="text-center underline pt-14 mx-auto font-semibold">
-        Show more
-      </button>
+      {isLoading && (
+        <div className="grid grid-cols-3 gap-4">
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+        </div>
+      )}
+      {status === "failed" && <FailureToFetchProducts />}
+      {status === "succeeded" && <Products />}
     </section>
   );
 }
