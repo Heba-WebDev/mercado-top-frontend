ce8d70b5c1e112ee123345553af1a7a8ed80c51b
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-27 13:09:43 +0300

    Changed the baseUrl to localhost for testing purposes

--- app/react-query/axios.tsx
+++ app/react-query/axios.tsx
@@ -3,5 +3,6 @@ import axios from "axios";
 const URL = process.env.BASE_URL as string;
 
 export const ApiAxiosInterceptor = axios.create({
-  baseURL: "https://mercado-top.onrender.com",
+  //baseURL: "https://mercado-top.onrender.com",
+  baseURL: "http://localhost:3001",
 });
