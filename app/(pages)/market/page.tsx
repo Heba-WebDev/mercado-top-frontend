a015863858f71d0250a6fa283f43ead136930438
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-27 13:05:18 +0300

    Removed unused imports

--- app/(pages)/market/page.tsx
+++ app/(pages)/market/page.tsx
@@ -1,6 +1,4 @@
-import AddAProduct from "./components/AddAProduct";
 import ProductsList from "./components/ProductsList";
-import SearchProducts from "./components/SearchProducts";
 
 export default function Productos() {
   return (
