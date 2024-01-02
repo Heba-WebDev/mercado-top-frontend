4db54f737aa952a30f2c5d8825b595e2bda98a9b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-02 14:03:57 +0300

    Removed username and bio from type IModifySettings and interface FormValues, as they are yet to be implemented on the backend

--- app/(pages)/(user)/settings/types-interfaces/index.ts
+++ app/(pages)/(user)/settings/types-interfaces/index.ts
@@ -1,12 +1,23 @@
 export interface FormValues {
   user_id: string;
   name: string | null;
-  username: string | null;
+  //username: string | null;
   email: string | null;
   password: string | null;
-  bio: string | null;
+  //bio: string | null;
   country: string | null;
-  profile_img: File | null;
+  profile_picture: File | null;
+}
+
+export type IModifySettings = {
+  user_id: string;
+  name: string | null;
+  //username: string | null;
+  email: string | null;
+  password: string | null;
+  //bio: string | null;
+  country: string | null;
+  profile_picture: File | null;
 }
 
 
