3109ac5ca6a513175bdd253820b35f860b9c0572
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-02 14:02:36 +0300

    Added inital values, added cancel button to clear made changes and save changes fully works now

--- app/(pages)/(user)/settings/components/AccountSettings.tsx
+++ app/(pages)/(user)/settings/components/AccountSettings.tsx
@@ -3,29 +3,40 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { countryList } from "@/app/utils/ListOfCountries";
 import Select from "react-select";
-import { FormValues } from "../types-interfaces";
+import { FormValues, IModifySettings } from "../types-interfaces";
 import { Form, Formik } from "formik";
 import Spinner from "@/app/components/globals/Spinner";
-import { useAppSelector } from "@/app/hooks/store";
+import { useAppSelector, useAppDispatch } from "@/app/hooks/store";
 import { IMAGE_URL } from "@/app/utils/ImageBaseURL";
 import DeleteAccount from "./DeleteAccount";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { modifySettings } from "../api-settings";
+import { CustomError } from "@/app/utils/CustomError";
+import { updateUser } from "@/app/store/users/slice";
+
 export default function AccountSettings() {
   const user = useAppSelector((state) => state.users);
   const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState(false);
   const id = Date.now().toString();
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { mutateAsync } = useMutation({
+    mutationFn: modifySettings,
+  });
   const initialValues: FormValues = {
     user_id: "",
-    name: "",
-    username: "",
-    email: "",
+    name: user[0].name,
+    //username: "",
+    email: user[0].email,
     password: "",
-    bio: "",
-    country: "",
-    profile_img: null,
+    //bio: "",
+    country: user[0].country,
+    profile_picture: null,
   };
-
   const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.currentTarget.files && event.currentTarget.files.length > 0) {
       const file = event.currentTarget.files[0];
@@ -37,7 +48,34 @@ export default function AccountSettings() {
     }
   };
   useEffect(() => setIsMounted(true), []);
-  const handleSubmit = () => {};
+  const handleSubmit = async (values: IModifySettings) => {
+    if (user && user[0] && user[0].user_id) {
+      values.user_id = user[0].user_id;
+    } else {
+      console.error("user_id is not defined");
+      return;
+    }
+    const formData = new FormData();
+    Object.keys(values).forEach((key) => {
+      const indexableValues = values as { [key: string]: any };
+      formData.append(key, indexableValues[key]);
+    });
+
+    try {
+      const res = await mutateAsync(formData);
+      toast.success(res?.message);
+      dispatch(updateUser(res?.data?.usr));
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        const customError = error as CustomError;
+        if (customError.isCustomError) {
+          toast(customError.response.data.message);
+        } else {
+          console.error(customError.message);
+        }
+      }
+    }
+  };
   return (
     <section className="max-w-[750px] mx-auto mt-6">
       <h2 className="text-lg font-semibold">Account Settings</h2>
@@ -54,9 +92,10 @@ export default function AccountSettings() {
                 <Image
                   src={selectedAvatar}
                   alt="avatar"
-                  width={60}
-                  height={30}
-                  className="rounded-full"
+                  width={50}
+                  height={50}
+                  style={{ height: "60px", width: "60px" }}
+                  className="rounded-full object-cover"
                 />
               )}
               {!selectedAvatar && (
@@ -71,25 +110,24 @@ export default function AccountSettings() {
                     alt=""
                     width={80}
                     height={80}
-                    objectFit="cover"
-                    style={{ height: "80px", width: "80px" }}
+                    style={{ height: "60px", width: "60px" }}
                     className="rounded-full object-cover"
                   />
                 </>
               )}
-              <label htmlFor="profile_img" className="hidden">
+              <label htmlFor="profile_picture" className="hidden">
                 Upload a new avatar
               </label>
               <div className=" flex flex-col-reverse items-center gap-2">
                 <input
-                  id="profile_img"
-                  name="profile_img"
+                  id="profile_picture"
+                  name="profile_picture"
                   type="file"
                   accept="image/*"
                   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                     if (event.currentTarget.files) {
                       formik.setFieldValue(
-                        "profile_img",
+                        "profile_picture",
                         event.currentTarget.files[0]
                       );
                     }
@@ -100,7 +138,8 @@ export default function AccountSettings() {
                 <button
                   type="button"
                   onClick={() => {
-                    const inputElement = document.getElementById("profile_img");
+                    const inputElement =
+                      document.getElementById("profile_picture");
                     if (inputElement) {
                       inputElement.click();
                     }
@@ -110,7 +149,11 @@ export default function AccountSettings() {
                   Update
                 </button>
               </div>
-              <button type="button">
+              <button
+                type="button"
+                aria-label="Remove avatar"
+                onClick={() => setSelectedAvatar(null)}
+              >
                 <Image
                   src="/images/globals/remove.svg"
                   alt="remove uploaded avatar"
@@ -137,8 +180,8 @@ export default function AccountSettings() {
                 id="username"
                 name="username"
                 type="text"
-                onChange={formik.handleChange}
-                value={formik.values.username as string}
+                //onChange={formik.handleChange}
+                //value={formik.values.username as string}
                 className="border py-3  px-2 rounded-lg focus:outline-[#33A077]"
               />
             </div>
@@ -191,8 +234,8 @@ export default function AccountSettings() {
               <textarea
                 id="bio"
                 name="biod"
-                onChange={formik.handleChange}
-                value={formik.values.bio as string}
+                //onChange={formik.handleChange}
+                //value={formik.values.bio as string}
                 className="border py-3 resize-none  px-2 rounded-lg focus:outline-[#33A077]"
               />
             </div>
@@ -251,13 +294,26 @@ export default function AccountSettings() {
             <div className="flex items-center gap-2">
               <button
                 type="submit"
-                className=" bg-[#33A077] hover:bg-[#227356] px-4 py-2 text-white rounded-full"
+                className={`${formik.isSubmitting ? "" : `bg-[#33A077]`} ${
+                  formik.isSubmitting ? "" : "hover:bg-[#227356]"
+                } px-4 py-2 text-white rounded-full`}
               >
                 {formik.isSubmitting ? <Spinner /> : "Save Changes"}
               </button>
-              <button type="button" className=" border px-3 py-2 rounded-full">
-                Cancel
-              </button>
+              {formik.isSubmitting ? (
+                ""
+              ) : (
+                <button
+                  type="button"
+                  className=" border px-3 py-2 rounded-full"
+                  onClick={() => {
+                    formik.resetForm();
+                    setSelectedAvatar(null);
+                  }}
+                >
+                  Cancel
+                </button>
+              )}
             </div>
             <DeleteAccount />
           </Form>
