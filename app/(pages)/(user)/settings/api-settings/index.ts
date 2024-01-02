877ccc92124e9299cb4e49da340a927d4244dcca
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-02 14:00:49 +0300

    Created modifySettings function to send changes made by user to the backend

--- app/(pages)/(user)/settings/api-settings/index.ts
+++ app/(pages)/(user)/settings/api-settings/index.ts
@@ -1,7 +1,24 @@
 import axios from "axios";
 import { CustomError } from "@/app/utils/CustomError";
 import { ApiAxiosInterceptor } from "@/app/react-query/axios";
-import { IDeleteAccount } from "../types-interfaces";
+import { IDeleteAccount, IModifySettings } from "../types-interfaces";
+
+export const modifySettings = async(data: FormData) => {
+    try {
+        const response = await ApiAxiosInterceptor.put("/api/users/updateUser", data);
+        if (response.status >= 200 && response.status < 300) {
+      return response.data;
+    } else {
+      throw new CustomError(response);
+    }
+    } catch(error) {
+        if (axios.isAxiosError(error) && error?.response) {
+      throw new CustomError(error.response);
+    } else {
+      throw error;
+    }
+    }
+}
 
 export const deleteAccount = async(data: IDeleteAccount) => {
     try {
