6fd0e6010be81718dd05b3b1dc980e25716e7e0c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-05 19:16:06 +0300

    Modified the signup image to algin it correctly

--- app/(pages)/(auth)/signup/components/SignupImage.tsx
+++ app/(pages)/(auth)/signup/components/SignupImage.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function SignUpImage() {
   return (
-    <section className=" bg-green-50 hidden max-h-[70%] md:grid justify-center items-center">
-      <div className="flex max-w-2xl ">
+    <section className=" bg-green-50 hidden h-full md:grid justify-center">
+      <div className="flex justify-center items-center max-w-2xl max-h-[70%]">
         <Image
           src="/images/globals/signup-img.svg"
           alt=""
           width={450}
           height={350}
+          style={{ width: "450px", height: "auto" }}
         />
       </div>
     </section>
