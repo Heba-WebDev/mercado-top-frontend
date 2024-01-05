9b2eaf5a604778c2334c3753415cfbf9afb0300b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-05 18:57:04 +0300

    Created a component that gives the user the option to sign up with a social account

---
+++ app/(pages)/(auth)/signup/components/Oauth.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function OauthSignup() {
+  return (
+    <section
+      className={`flex items-center gap-4 border
+      mt-6 mb-4 py-3 justify-center rounded-lg border-opacity-25`}
+    >
+      <Image
+        src="/images/globals/GoogleIcon.svg"
+        alt="mercado top"
+        width={30}
+        height={30}
+        style={{ width: "25px", height: "25px" }}
+      />
+      <p className="font-bold">Sign up with Google</p>
+    </section>
+  );
+}
