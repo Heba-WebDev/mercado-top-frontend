53b0e4a9d4c4daeebe7cd5b813f55d048f798bfa
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-06 15:52:53 +0300

    Changed alignment of the image

--- app/(pages)/(auth)/signup/components/SignupImage.tsx
+++ app/(pages)/(auth)/signup/components/SignupImage.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function SignUpImage() {
   return (
-    <section className=" bg-green-50 hidden h-full md:grid justify-center">
-      <div className="flex justify-center items-center max-w-2xl max-h-[70%]">
+    <section className=" bg-green-50 hidden h-full lg:grid justify-center items-center">
+      <div className="flex justify-center items-center max-w-2xl">
         <Image
           src="/images/globals/signup-img.svg"
           alt=""
           width={450}
           height={350}
-          style={{ width: "450px", height: "auto" }}
+          style={{ width: "100%", height: "auto" }}
         />
       </div>
     </section>
