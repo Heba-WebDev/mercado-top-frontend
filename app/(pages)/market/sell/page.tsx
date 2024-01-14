1a22c391f2748a07755358422b6a5cc50643fc19
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-14 14:28:47 +0300

    Modified the sell item page to adapt the new UI

--- app/(pages)/market/sell/page.tsx
+++ app/(pages)/market/sell/page.tsx
@@ -1,17 +1,18 @@
 import SellForm from "./components/Form";
+import ProductPreview from "./components/ProductPreview";
 
 export default function Sell() {
   return (
-    <main className="container mx-auto py-24 px-2 grid gap-4 md:justify-center">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-4xl md:text-5xl font-bold">
-          You no longer need it?
+    <main className="py-24 px-2 md:px-0 grid ">
+      <div className="flex flex-col gap-4 border border-x-0 border-t-0 pb-8 w-full">
+        <h1 className="container mx-auto text-4xl font-semibold">
+          Add New Product
         </h1>
-        <p className=" text-center">
-          Sell any item you have at home on Mercado Top
-        </p>
       </div>
-      <SellForm />
+      <div className="container mx-auto grid lg:grid-cols-2 gap-x-6">
+        <SellForm />
+        <ProductPreview />
+      </div>
     </main>
   );
 }
