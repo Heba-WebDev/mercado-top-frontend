2685d4aff9e36b4cae91f227714f21ed5ddbd44b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-19 19:24:47 +0300

    Added a state to keep track of images the user uploaded in add new product form

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./users/slice";
 import productReducer from "./products/slice"
+import productPreview from "./productPreview";
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
-        products: productReducer
+        products: productReducer,
+        productPreviw: productPreview
     },
 });
 
