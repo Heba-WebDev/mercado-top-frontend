d7d1f929026870b87a85707f2eeca425a70c02de
Heba-Webdev23 <jtvonline@yahoo.com>
2024-02-11 14:47:46 +0300

    Modified skeleton card to match current ui

--- app/(pages)/market/components/SkeletonCard.tsx
+++ app/(pages)/market/components/SkeletonCard.tsx
@@ -1,15 +1,13 @@
 export default function SkeletonCard() {
   return (
     <section className="relative flex flex-col gap-4 border p-4 rounded z-50 shadow-md w-full bg-gray-50 animate-pulse">
-      <div className="h-64 bg-gray-400 rounded"></div> {/* Image placeholder */}
       <h2 className="h-6 bg-gray-400 rounded w-3/4"></h2>{" "}
+      <h3 className="h-6 bg-gray-400 rounded-full w-24"></h3>{" "}
+      <div className="h-64 bg-gray-400 rounded"></div> {/* Image placeholder */}
       {/* Title placeholder */}
       <div className="flex justify-between">
-        <div className="h-5 w-5 bg-gray-400 rounded-full"></div>{" "}
         {/* Icon placeholder */}
-        <div className="h-5 w-5 bg-gray-400 rounded-full"></div>{" "}
         {/* Icon placeholder */}
-        <div className="h-5 w-5 bg-gray-400 rounded-full"></div>{" "}
         {/* Icon placeholder */}
       </div>
     </section>
