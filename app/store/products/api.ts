49a9ad59ae3218e9af4dd2f2d30b8942ea87cf2b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-02-11 14:49:56 +0300

    Added a fetching function for getProductByCategoryLocation

--- app/store/products/api.ts
+++ app/store/products/api.ts
@@ -3,9 +3,31 @@ import axios from "axios";
 import { CustomError } from "@/app/utils/CustomError";
 import { ApiAxiosInterceptor } from "@/app/react-query/axios";
 
+type ISearchProductsByCategoryLocation = {
+  category: string;
+  country: string;
+}
+
 export const getAllProducts = async () => {
   try {
     const response = await ApiAxiosInterceptor.get("/api/products");
+    if (response.status >= 200 && response.status < 300) {
+      return response.data;
+    } else {
+      throw new CustomError(response);
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error) && error?.response) {
+      throw new CustomError(error.response);
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const getProductByCategoryLocation = async (data: ISearchProductsByCategoryLocation) => {
+  try {
+    const response = await ApiAxiosInterceptor.get("/api/products/byCategoryLocation", {data});
     if (response.status >= 200 && response.status < 300) {
       return response.data;
     } else {
