48dc70e952ca12dbc53671380a92522b822a7ec6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-02-11 14:49:11 +0300

    Added searchProducts state

--- app/store/index.ts
+++ app/store/index.ts
@@ -3,13 +3,15 @@ import userReducer from "./users/slice";
 import productReducer from "./products/slice"
 import productPreview from "./productPreview";
 import pagination from "./pagination";
+import searchProducts from "./searchProducts"
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
         products: productReducer,
         productPreviw: productPreview,
-        pagination: pagination
+        pagination: pagination,
+        searchProducts: searchProducts
     },
 });
 
